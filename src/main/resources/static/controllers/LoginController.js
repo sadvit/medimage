b845f9e8a187ac8f5408bf3032c04d3c69111b78
vitaly.sadovskiy <vitaly.sadovskiy@mainsoft.by>
2015-11-28 09:27:35 +0300

    Static removed

--- src/main/resources/static/controllers/LoginController.js
+++
@@ -1,9 +0,0 @@
-angular.module('medimage').controller('loginController', ['$scope', 'loginService', function ($scope, loginService) {
-
-    $scope.auth = function () {
-        loginService.auth($scope.login, $scope.pass, function (response) {
-            console.log(response);
-        });
-    };
-
-}]);
--- src/main/resources/static/services/ProcessService.js
+++
@@ -1,9 +0,0 @@
-angular.module('medimage').service('processService', ['Restangular', function (Restangular) {
-
-    this.binary = function (id, params, callback) {
-        Restangular.one('process/binary', id).customPOST(params).then(function (data) {
-            callback(data.id);
-        });
-    };
-
-}]);
