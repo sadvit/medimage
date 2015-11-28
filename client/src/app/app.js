ed69825aeec342f342d60ec7e6d2654ee46b66cf
vitaly.sadovskiy <vitaly.sadovskiy@mainsoft.by>
2015-11-28 11:17:14 +0300

    Test client added

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,4 +1,18 @@
+'use strict';
 
-angular.module('medimage', [
-  'ngRoute'
-]);
+var medimage = angular.module('medimage', ['restangular', 'ui.router']);
+
+medimage.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("login");
+    $stateProvider
+        .state('login', {
+            url: "/login",
+            templateUrl: "assets/login/login.html",
+            controller: "loginController"
+        })
+        .state('test', {
+            url: "/test",
+            templateUrl: "assets/test/test.html",
+            controller: "testController"
+        });
+});
