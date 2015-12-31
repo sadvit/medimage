3e7d2d2746e5dbfa39eec20354bdc498e52148cb
vitaly.sadovskiy <vitaly.sadovskiy@mainsoft.by>
2015-12-31 13:40:02 +0300

    Recognize logic added

--- server/src/main/java/com/sadvit/controllers/RecognizeController.java
+++ server/src/main/java/com/sadvit/controllers/RecognizeController.java
@@ -1,16 +1,22 @@
 package com.sadvit.controllers;
 
+import com.sadvit.services.ImageService;
+import com.sadvit.utils.FileUtils;
 import org.neuroph.core.NeuralNetwork;
 import org.neuroph.core.data.DataSet;
-import org.neuroph.core.data.DataSetRow;
+import org.neuroph.imgrec.FractionRgbData;
+import org.neuroph.imgrec.ImageRecognitionHelper;
 import org.neuroph.imgrec.ImageRecognitionPlugin;
-import org.neuroph.nnet.Perceptron;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.*;
-import java.awt.image.BufferedImage;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by vitaly.sadovskiy.
@@ -19,30 +25,35 @@ import java.awt.image.BufferedImage;
 @RequestMapping("/recognize")
 public class RecognizeController {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public void test() {
-		// create new perceptron network
-		NeuralNetwork neuralNetwork = new Perceptron(2, 1);
-		// create training set
-		DataSet trainingSet =
-				new DataSet(2, 1);
-		// add training data to training set (logical OR function)
-		trainingSet. addRow (new DataSetRow(new double[]{0, 0},
-				new double[]{0}));
-		trainingSet. addRow (new DataSetRow (new double[]{0, 1},
-				new double[]{1}));
-		trainingSet. addRow (new DataSetRow (new double[]{1, 0},
-				new double[]{1}));
-		trainingSet. addRow (new DataSetRow (new double[]{1, 1},
-				new double[]{1}));
-		// learn the training set
-		neuralNetwork.learn(trainingSet);
-		// save the trained network into file
-		neuralNetwork.save("or_perceptron.nnet");
+	@Autowired
+	private ImageService imageService;
+
+	@RequestMapping(method = RequestMethod.POST)
+	public void teach(Map<String, String> images) {
+		InputStream inputStream = FileUtils.readFile("/home/sadvit/Documents/content/sadvit/networks/NetworkLabel.nnet");
+		NeuralNetwork network = NeuralNetwork.load(inputStream);
+		DataSet trainingSet = ImageRecognitionHelper.createTrainingSet(extractLabels(images), extractRgbData(images));
+		network.learn(trainingSet);
 	}
 
-	public void test2(BufferedImage image) {
-		Image instance = image.getScaledInstance(400, 400, Image.SCALE_SMOOTH);
+	@RequestMapping(method = RequestMethod.GET)
+	public Map<String, Double> recognize(List<String> images) {
+		InputStream inputStream = FileUtils.readFile("/home/sadvit/Documents/content/sadvit/networks/NetworkLabel.nnet");
+		NeuralNetwork network = NeuralNetwork.load(inputStream);
+		ImageRecognitionPlugin imageRecognition = (ImageRecognitionPlugin)network.getPlugin(ImageRecognitionPlugin.class); // get the image recognition plugin from neural network
+		Map<String, Double> result = new HashMap<>();
+		images.forEach(id -> result.putAll(imageRecognition.recognizeImage(imageService.getBufferedImage(id))));
+		return result;
+	}
+
+	public List<String> extractLabels(Map<String, String> images) {
+		return images.values().stream().collect(Collectors.toList());
+	}
+
+	public Map<String, FractionRgbData> extractRgbData(Map<String, String> images) {
+		Map<String, FractionRgbData> dataMap = new HashMap<>();
+		images.forEach((s, s2) -> dataMap.put(s, new FractionRgbData(imageService.getBufferedImage(s2))));
+		return dataMap;
 	}
 
 }
