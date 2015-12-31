e2f4fd85c7a07c74fdf57d69a0e0bbf6d6dbcceb
vitaly.sadovskiy <vitaly.sadovskiy@mainsoft.by>
2015-12-31 13:41:59 +0300

    Recognize logic added

---
+++ server/src/main/java/com/sadvit/services/RecognizeService.java
@@ -0,0 +1,24 @@
+package com.sadvit.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by sadvit on 30.12.15.
+ */
+@Service
+public class RecognizeService {
+
+    public void teach(Map<String, String> images) {
+
+    }
+
+    public Map<String, String> recognize(List<String> images) {
+        return null;
+    }
+
+}
