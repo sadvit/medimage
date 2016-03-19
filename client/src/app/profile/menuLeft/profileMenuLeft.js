85c9892b66cd16170d7903e28f79d12936c6203c
sadvit <mr.sadvit@gmail.com>
2016-03-19 11:39:40 +0300

    User profile page was added

---
+++ client/src/app/profile/menuLeft/profileMenuLeft.js
@@ -0,0 +1,12 @@
+angular.module('medimage').directive('profileMenuLeft', [function () {
+  return {
+    link: function (scope, element, attrs) {
+
+    },
+    templateUrl: 'profile/menuLeft/menuLeft.html',
+    restrict: 'A',
+    controller: ['$scope','modalsService', function ($scope, modalsService) {
+
+    }]
+  }
+}]);
